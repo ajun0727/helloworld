b9b5fa7db207d650f273dcc2e500e41827e1053e
ajun0727 <1332212033@qq.com>
2018-12-09 20:07:38 +0800

    日志

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+/**
+ * Created by GuoJunJun on 2018-12-7.
+ */
+public class HelloWorld {
+        public static void main(String[] args) {
+            System.out.println("Helo World!");
+        }
+}
